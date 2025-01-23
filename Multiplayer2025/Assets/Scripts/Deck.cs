5d44119bbb9f3687d3d0056e78e2a298e19901b4
João Vitor Ribeiro <2015ribeirojv@gmail.com>
2025-01-23 16:39:04 -0300

    scene

---
+++ Multiplayer2025/Assets/Scripts/Deck.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Deck : MonoBehaviour
+{
+    public List<Card> deck = new List<Card>(); //lista de cartas do deck
+    public Transform handArea;
+    public GameObject cardPrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
